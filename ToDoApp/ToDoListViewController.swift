cf8e30bd6f26a3c7d6d26feb768c9e0c53ba7b05
Xia Tran <xiahtran@gmail.com>
2019-03-19 14:46:05 +0000

    Add new item functionality via an alert, complete.

--- ToDoApp/ToDoListViewController.swift
+++ ToDoApp/ToDoListViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
+  
 
-  let itemArray = ["do this 1", "do this 2", "do this 3"]
+  var itemArray = ["do this 1", "do this 2", "do this 3"]
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -32,22 +33,52 @@ class ToDoListViewController: UITableViewController {
 
   //MARK: - tableView Delegate Methods
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    //print(itemArray[indexPath.row])
     
-   
+    //print(itemArray[indexPath.row])
     if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
       tableView.cellForRow(at: indexPath)?.accessoryType = .none
     } else {
       //gives a checkmark using the accessoryType
       tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-      
     }
-    
     //highlights and then deselects the selected row
     tableView.deselectRow(at: indexPath, animated: true)
     
-    
   }
 
+  //MARK: - Add New Items To List
+  
+  @IBAction func addButton(_ sender: UIBarButtonItem) {
+    
+    var textField = UITextField()
+    
+    let alert = UIAlertController(title: "Add a new ToDo Item to your list", message: "", preferredStyle: .alert)
+    
+    //in the alert box, a text field is there. has a placeholder. prints the text.
+    alert.addTextField { (alertTextField) in
+      alertTextField.placeholder = "Create new item"
+      textField = alertTextField
+    }
+    
+    //the option given - add item, if selected, will print success
+    let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+      print(textField.text!)
+      print("success")
+      
+      //add new text to array and reload list to update
+      self.itemArray.append(textField.text!)
+      self.tableView.reloadData()
+    }
+    
+
+
+    
+    //give the alert box an option to choose
+    alert.addAction(action)
+    
+    //present the alert
+    present(alert, animated: true, completion: nil)
+  }
+  
 }
 
